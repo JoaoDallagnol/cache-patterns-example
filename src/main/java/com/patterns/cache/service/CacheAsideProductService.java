a8c48e0de51d5a4e8bdcb4b18319df0cd5b4a8be
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-03-17 10:50:04 -0300

    Adding orElseThrow in service

--- src/main/java/com/patterns/cache/service/CacheAsideProductService.java
+++ src/main/java/com/patterns/cache/service/CacheAsideProductService.java
@@ -3,6 +3,7 @@ package com.patterns.cache.service;
 import com.patterns.cache.dto.ProductResponse;
 import com.patterns.cache.entity.Product;
 import com.patterns.cache.entity.ProductCache;
+import com.patterns.cache.exception.ProductNotFoundException;
 import com.patterns.cache.mapper.ProductMapper;
 import com.patterns.cache.repository.ProductCacheRepository;
 import com.patterns.cache.repository.ProductRepository;
@@ -26,7 +27,7 @@ public class CacheAsideProductService {
             return mapper.toResponseFromCache(cached.get());
         }
 
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
         cacheRepository.save(mapper.toCache(product));
         return mapper.toResponse(product);
     }
