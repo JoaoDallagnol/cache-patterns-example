ce7973c8b1a5d0f548b0a6e78d3eb6e117151d86
JoaoDallagnol <jvdallagnol2001@gmail.com>
2026-03-17 09:32:34 -0300

    Implementing service logic for write-through and cache-aside

--- src/main/java/com/patterns/cache/service/CacheAsideProductService.java
+++ src/main/java/com/patterns/cache/service/CacheAsideProductService.java
@@ -1,27 +1,34 @@
 package com.patterns.cache.service;
 
 import com.patterns.cache.dto.ProductResponse;
+import com.patterns.cache.entity.Product;
+import com.patterns.cache.entity.ProductCache;
 import com.patterns.cache.mapper.ProductMapper;
+import com.patterns.cache.repository.ProductCacheRepository;
 import com.patterns.cache.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CacheAsideProductService {
 
-    private final ProductRepository productRepository;
+    private final ProductRepository repository;
+    private final ProductCacheRepository cacheRepository;
     private final ProductMapper mapper;
 
     public ProductResponse getById(Long id) {
-        // try get from cache
-        // if not exists in cache
-        // get from database
-        // save to cache
-        // return result
-        return null;
+        Optional<ProductCache> cached = cacheRepository.findById(id);
+        if (cached.isPresent()) {
+            return mapper.toResponseFromCache(cached.get());
+        }
+
+        Product product = repository.findById(id).get();
+        cacheRepository.save(mapper.toCache(product));
+        return mapper.toResponse(product);
     }
 
     public List<ProductResponse> getAll() {
--- src/main/java/com/patterns/cache/service/WriteThroughService.java
+++ src/main/java/com/patterns/cache/service/WriteThroughService.java
@@ -2,7 +2,9 @@ package com.patterns.cache.service;
 
 import com.patterns.cache.dto.ProductRequest;
 import com.patterns.cache.dto.ProductResponse;
+import com.patterns.cache.entity.Product;
 import com.patterns.cache.mapper.ProductMapper;
+import com.patterns.cache.repository.ProductCacheRepository;
 import com.patterns.cache.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,11 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class WriteThroughService {
-    private final ProductRepository productRepository;
+    private final ProductRepository repository;
+    private final ProductCacheRepository productCacheRepository;
     private final ProductMapper mapper;
 
     public ProductResponse create(ProductRequest request) {
-        return null;
+        Product product = repository.save(mapper.toEntity(request));
+        productCacheRepository.save(mapper.toCache(product));
+
+        return mapper.toResponse(product);
     }
 
     public ProductResponse update(Long id, ProductRequest request) {
